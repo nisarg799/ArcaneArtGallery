2a91790830ecb0e056fd3047ae0ecaf1adac2655
Kenil Chovatiya <68478360+Kenil12@users.noreply.github.com>
2021-04-09 15:47:31 -0400

    ecommerscewebsite submitted

---
+++ Arcane_Art_Gallery/about.php
@@ -0,0 +1,27 @@
+ <!-- Masthead-->
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end mb-4" style="background: #0000002e;">
+                    	 <h1 class="text-uppercase text-white font-weight-bold">About Us</h1>
+                        <hr class="divider my-4" />
+                    </div>
+                    
+                </div>
+            </div>
+        </header>
+
+    <section class="page-section">
+        <div class="container">
+         
+            
+
+        <p> <h1> <b> ABOUT US </b> </h1> <br>
+		The <b> ARCANE ART GALLERY </b> is home to more than 1000 works of arts. The gallery has one of the finest collection of Indigenous art along with the masterworks from many other artisitc traditions. While remaining comitted to core values of art that includes tradition, outreach and the sharing of the knowledge, the <b> ARCANE ART GALLERY </b> is very alert to accept new ways that connects itself to audience, while demonstrating the interest of art to this and the coming generations. 
+		</p>  
+		<p> The <b> ARCANE ART GALLERY </b> has been the gateway for the visual arts and is responsible for acquiring ,preserving and exhibiting works of all the categories of paintings. </p>
+            
+			<p> The <b>ARCANE ART GALLERY</b> will be welcoming all paintings that tell stories that reflect community and generates meaning to the lives of their customers. The gallery also welcomes the most vital and contemporary art from all around the world. </p>
+            
+        </div>
+        </section>
